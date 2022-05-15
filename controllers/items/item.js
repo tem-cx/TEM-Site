b1f6643ce86f5d7f5e74130d80346e77374d9ad6
BYTE <ByteGamerYT@gmail.com>
2022-05-15 18:37:40 +0200

    prety much finished all of item player history

--- controllers/items/item.js
+++ controllers/items/item.js
@@ -3,18 +3,11 @@ const axios = require('axios').default;
 exports.getItems = async (req, res) => {
     const params = req.params
 
-
-
-
-
     if (params[0].length != 36) {
         const { search } = req.body
         return res.status(200).redirect(`/`)
     }
     
-
-
-
     res.status(200).render('items/item',{
         search: params[0]
     
