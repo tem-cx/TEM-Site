bdc7bb84a9d86d58d2b074c7e325ed7b0840a113
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2022-05-26 17:46:46 +0100

    add direct redirect for the discord

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,9 @@ const router = Router()
 router.use('/', home)
 router.use('/items/', item)
 
+router.get('/discord', (req, res) => {
+    res.redirect("https://discord.com/invite/bUE3r3Jckc")
+})
 
 router.get('*', (req, res) => {
     res.status(404).send('errors/404.ejs')
